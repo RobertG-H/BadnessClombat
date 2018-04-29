72eabc20126fcb4f64cdd06cc41555e11e08346f
RobertG-H <ultraconga@hotmail.com>
2018-04-28 23:45:17 -0400

    Initial commit. Moving sword in three directions

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public string _horizontal2, _vertical2;
+
+    private Transform sword;
+    //private GameObject sword;
+    private float armRotation;
+
+    private const int IDLE = 0;
+    private const int FORWARD = 1;
+    private const int UP = 2;
+    private const int BACK = 3;
+
+    private int currentState;
+    // Use this for initialization
+    void Start () {
+        sword = this.gameObject.transform.GetChild(0);
+        currentState = IDLE;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(Input.GetAxisRaw(_horizontal2));
+        if (Input.GetAxisRaw(_horizontal2) == 0 && Input.GetAxisRaw(_vertical2) == 0)
+        {
+            // Go to neutral position
+           sword.GetComponent<Sword>().move(currentState, IDLE);
+            currentState = IDLE;
+        }
+        else if (Input.GetAxisRaw(_horizontal2) > 0.9f)
+        {
+            //armRotation = Mathf.Atan2(Input.GetAxisRaw(_vertical2), Input.GetAxisRaw(_horizontal2)) * 360 / 2 / Mathf.PI;
+            sword.GetComponent<Sword>().move(currentState, FORWARD);
+            currentState = FORWARD;
+        }
+        else if (Input.GetAxisRaw(_vertical2) > 0.9f)
+        {
+            sword.GetComponent<Sword>().move(currentState, UP);
+            currentState = UP;
+        }
+        else if (Input.GetAxisRaw(_horizontal2) < -0.9f)
+        {
+            sword.GetComponent<Sword>().move(currentState, BACK);
+            currentState = BACK;
+        }
+
+        //transform.GetChild(0).eulerAngles = new Vector3(0, transform.GetChild(0).eulerAngles.y, armRotation);
+    }
+}
---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour {
+
+    public float _speed;
+    public float _rotationSpeed;
+    public GameObject[] _myWaypoints; // to define the movement waypoints
+    private Rigidbody2D rb;
+
+    private Vector3[] ANGLES = new Vector3[] { new Vector3(0,0,90), new Vector3(0, 0, 0), new Vector3(0, 0, 0), new Vector3(0, 0, 180) };
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void move(int from, int to)
+    {
+        //Debug.Log("Sdas");
+        if(transform.position == _myWaypoints[to].transform.position)
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(rb.position, _myWaypoints[to].transform.position, _speed * Time.deltaTime);
+            transform.eulerAngles = Vector3.RotateTowards(transform.eulerAngles, ANGLES[to],0.0f, _rotationSpeed);
+        }
+    }
+}
